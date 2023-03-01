0cc4361952b1faf946bdf9af0c961b863cba75a3
Mohit Mohlia <mohitmohlia96@gmail.com>
2023-03-01 12:11:29 +0530

    mocking jest

---
+++ src/components/counter-two/counter-two.tsx
@@ -0,0 +1,18 @@
+import { CounterTwoProps } from "./counter-two.types";
+
+const CounterTwo = (props: CounterTwoProps) => {
+  return (
+    <div>
+      <h1>Counter Two</h1>
+      <p>{props.count}</p>
+      {props.handleIncrement && (
+        <button onClick={props.handleIncrement}>Increment</button>
+      )}
+      {props.handleDecrement && (
+        <button onClick={props.handleDecrement}>Decrement</button>
+      )}
+    </div>
+  );
+};
+
+export default CounterTwo;
