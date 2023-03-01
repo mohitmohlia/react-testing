2bbe4c8f4f4081bb669734851ce1ea609b0d597c
Mohit Mohlia <mohitmohlia96@gmail.com>
2023-03-01 13:26:15 +0530

    mocking http

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Application } from "./components/Application/Application";
 import Counter from "./components/Counter/Counter";
 import { MuiMode } from "./components/mui/mui-mode";
 import Skills from "./components/Skills/Skills";
+import Users from "./components/users/users";
 import { AppProviders } from "./providers/app-providers";
 
 function App() {
@@ -12,7 +13,8 @@ function App() {
         {/* <Application /> */}
         {/* <Skills skills={["nextjs", "js", "css"]} /> */}
         {/* <Counter /> */}
-        <MuiMode />
+        {/* <MuiMode /> */}
+        <Users />
       </div>
     </AppProviders>
   );
